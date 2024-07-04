4ca6a43ea7bdbfae80dfec7f203a1f186b9671b1
Alejandro suarez <Alejandrovs.fsdev@gmail.com>
2024-07-04 20:59:18 -0300

    1.6: generador de tabla

--- 01-JS-DOM-APIs/JS/main.js
+++ 01-JS-DOM-APIs/JS/main.js
@@ -10,19 +10,19 @@ window.addEventListener("load", function () {
 function openSidebar() {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle("open");
-}
+};
 
 function closeSidebar() {
     const sidebar = document.getElementById('sidebar');
     if (sidebar.classList.contains("open")) {
         sidebar.classList.remove("open");
     }
-}
+};
 
 function errorCliente() {
     const aboutError = document.getElementById("txt");
     aboutError.style.backgroundColor = "red";
-}
+};
 
 const config = {
     url: "https://api.chucknorris.io/jokes/random",
@@ -33,6 +33,13 @@ const configRepositoriesAPI = {
     url: "https://api.github.com/search/repositories?q=",
     metodo: "GET"
 };
+
+const data = [
+    {producto: "remera", categoria: "ropa", precio: "$30000"},
+    {producto: "tomátes", categoria: "alimento", precio: "$1500"},
+    {producto: "paracetamol", categoria: "remedio", precio: "$5000"},
+    {producto: "cuadernilo", categoria: "libreria", precio: "$30000"}
+];
 
 const githubRepositories = (configRepositoriesAPI) => {
     return new Promise((resolve, reject) => {
@@ -78,11 +85,11 @@ const githubRepositories = (configRepositoriesAPI) => {
         cliente.open(configRepositoriesAPI.metodo, url);
         cliente.send();
     });
-}
+};
 
 const search = document.getElementById("search").addEventListener("click", function () {
     githubRepositories(configRepositoriesAPI);
-})
+});
 
 const llamadaAPI = (config) => {
     return new Promise((resolve, reject) => {
@@ -120,4 +127,44 @@ document.getElementById("btn").addEventListener("click", function () {
         llamadaAPI(config);
 });
 
+function tableBuilder (data) {
 
+    const existingTable = document.querySelector("table");
+    if (existingTable) {
+        existingTable.remove();
+    }
+    
+    const table = document.createElement("table");
+    const tableHead = document.createElement("thead");
+    const tableRow = document.createElement("tr");
+
+    for (let key in data[0]){
+        const tableHeader = document.createElement("th");
+        tableHeader.appendChild(document.createTextNode(key));
+        tableRow.appendChild(tableHeader);
+    }
+
+    tableHead.appendChild(tableRow);
+    table.appendChild(tableHead);
+
+    const tableBody = document.createElement("tbody");
+
+    for (let i = 0; i < data.length; i++) {
+        const dataRow = data[i];
+        const tableRow = document.createElement("tr");
+
+        for (let key in dataRow) {
+            const tableData = document.createElement("td");
+            tableData.appendChild(document.createTextNode(dataRow[key]));
+            tableRow.appendChild(tableData);
+            }
+
+        tableBody.appendChild(tableRow);
+    }
+    table.appendChild(tableBody);
+    document.body.appendChild(table);
+}
+
+const tableGenerator = document.getElementById("tableGenerator").addEventListener("click", function () {
+    tableBuilder(data);
+});
